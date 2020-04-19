1ac0e14eebcb817899b80d35b9a55e772844de62
Juan Cruz Ozores <juanozores97@gmail.com>
2020-04-19 18:56:17 -0300

    cambio en la logica general del programa
    construcción de piano como objeto con keys como propiedades del mismo + creación de objeto UI cuyos métodos se encargan del manejo de modificaciones de estilo en pantalla

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-document.addEventListener('DOMContentLoaded', piano);
+document.addEventListener('DOMContentLoaded', app);
 
-function piano() {
-	//Cada instancia de PianoKey tiene como propiedades una referencia a sus elementos html correspondientes (tanto visual como de audio )
+function app() {
+	//CLASES Y OBJETOS
 	class PianoKey {
+		//cada instancia de PianoKey tiene como propiedades una referencia a sus elementos html correspondientes (tanto visual como de audio )
 		constructor(note, key) {
-			this.note = note;
-			this.key = key;
 			this.keyElement = document.getElementById(key);
 			this.audioElement = document.getElementById(note);
 		}
-		get playNote() {
-			this.audioElement.currentTime = 0;
-			this.audioElement.play();
-		}
-		get addStyleToKey() {
-			this.keyElement.classList.add('activeKey');
-		}
-		get removeStyleFromKey() {
-			//solo aplicable una vez que se reprodujo el audioElement
-			this.audioElement.addEventListener('ended', () => this.keyElement.classList.remove('activeKey'));
-		}
 	}
 
-	//nombre de cada instancia de  PianoKey  refiere a la letra del teclado que la hace sonar
-	let A = new PianoKey('Ca', 'A');
-	let W = new PianoKey('Db', 'W');
-	let S = new PianoKey('Da', 'S');
-	let E = new PianoKey('Eb', 'E');
-	let D = new PianoKey('Ea', 'D');
-	let F = new PianoKey('Fa', 'F');
-	let T = new PianoKey('Gb', 'T');
-	let G = new PianoKey('Ga', 'G');
-	let Y = new PianoKey('Ab', 'Y');
-	let H = new PianoKey('Aa', 'H');
-	let U = new PianoKey('Bb', 'U');
-	let J = new PianoKey('Ba', 'J');
+	const piano = {
+		element: document.getElementById('piano'),
+		keys: {
+			//nombre de cada instancia de  PianoKey  refiere a la letra del teclado que la hace sonar
+			A: new PianoKey('Ca', 'A'),
+			W: new PianoKey('Db', 'W'),
+			S: new PianoKey('Da', 'S'),
+			E: new PianoKey('Eb', 'E'),
+			D: new PianoKey('Ea', 'D'),
+			F: new PianoKey('Fa', 'F'),
+			T: new PianoKey('Gb', 'T'),
+			G: new PianoKey('Ga', 'G'),
+			Y: new PianoKey('Ab', 'Y'),
+			H: new PianoKey('Aa', 'H'),
+			U: new PianoKey('Bb', 'U'),
+			J: new PianoKey('Ba', 'J')
+		},
+		playNote(keyName) {
+			piano.keys[keyName].audioElement.currentTime = 0;
+			piano.keys[keyName].audioElement.play();
+		}
+	};
 
-	const pianoKeys = [ A, W, S, E, D, F, T, G, Y, H, U, J ];
+	const UI = {
+		addStyleToKey(keyName) {
+			piano.keys[keyName].keyElement.classList.add('activeKey');
+		},
+		removeStyleFromKey(keyName) {
+			//eliminación del estilo en la tecla cuando termina el audio element relacionado con la misma
+			piano.keys[keyName].audioElement.addEventListener('ended', () => {
+				piano.keys[keyName].keyElement.classList.remove('activeKey');
+			});
+		}
+	};
 
+	//EVENTOS
 	//uso del piano clickeado una tecla:
-	for (let i = 0; i < pianoKeys.length; i++) {
-		pianoKeys[i].keyElement.addEventListener('click', () => {
-			pianoKeys[i].playNote;
-			pianoKeys[i].addStyleToKey;
-			pianoKeys[i].removeStyleFromKey;
-		});
-	}
+	piano.element.addEventListener('click', (ev) => {
+		let clickedKeyName = ev.target.id;
+		UI.addStyleToKey(clickedKeyName);
+		piano.playNote(clickedKeyName);
+		UI.removeStyleFromKey(clickedKeyName);
+	});
 
 	//uso del piano usando el teclado
 	document.addEventListener('keydown', (ev) => {
@@ -53,13 +60,11 @@ function piano() {
 			//para evitar sonidos raros al mantener pulsada una letra del teclado
 			return;
 		}
-		const pressedKey = ev.key.toUpperCase();
-		//se busca en el array PianoKeys el objeto cuyo valor de la propiedad "key" es igual al valor de "pressedKey"
-		const keyToPlay = pianoKeys.filter((pianoKey) => pianoKey.key == pressedKey);
-		if (keyToPlay.length == 1) {
-			keyToPlay[0].playNote;
-			keyToPlay[0].addStyleToKey;
-			keyToPlay[0].removeStyleFromKey;
+		const pressedKeyName = ev.key.toUpperCase();
+		if (pressedKeyName in piano.keys) {
+			UI.addStyleToKey(pressedKeyName);
+			piano.playNote(pressedKeyName);
+			UI.removeStyleFromKey(pressedKeyName);
 		}
 	});
 }
